2dd3984bb10686d193c40460438f2cab92223548
sshlpp <arsenii.tsesko@gmail.com>
2025-09-06 12:37:30 +0200

    all models was created

--- module3/eshop/models.py
+++ module3/eshop/models.py
@@ -10,3 +10,16 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
     stock = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Return(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    requested_at = models.DateTimeField(auto_now_add=True)
